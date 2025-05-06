0cdf82f3859f2b76bd4673449320d28a12909f59
berke <berkekartal35@gmail.com>
2025-05-06 18:52:52 +0300

    joined login and home pages, implemented the UI for the proposed figma design, proided login and signup button functionalities (other buttons are not done yet)

--- practice-app/frontend-web/src/App.js
+++ practice-app/frontend-web/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 import WasteLog from './components/WasteLog';
 import Profile from './components/Profile';
+import LoginPage from './components/LoginPage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/waste" element={<WasteLog />} />
