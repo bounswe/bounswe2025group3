e4c56d69858ff1b7c01b26ebdf0a53942137ee82
omerfaruqb <omerfarukbayram365@gmail.com>
2025-05-14 15:08:56 +0300

    fix: waste log error display

--- practice-app/backend/apps/challenges/signals.py
+++ practice-app/backend/apps/challenges/signals.py
@@ -3,9 +3,9 @@ from django.dispatch import receiver
 from apps.waste.models import WasteLog
 from .services import ChallengeService
 
-@receiver(post_save, sender=WasteLog)
-def update_challenge_progress_on_wastelog(sender, instance, created, **kwargs):
-    if not created:
-        return  # Only act on new entries
+# @receiver(post_save, sender=WasteLog)
+# def update_challenge_progress_on_wastelog(sender, instance, created, **kwargs):
+#     if not created:
+#         return  # Only act on new entries
 
-    ChallengeService.track_progress(user=instance.user)
+#     ChallengeService.track_progress(user=instance.user)
