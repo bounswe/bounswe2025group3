6d270a421870e5d84c2253ab5633cc71582fffd0
nilsutuysuz <162128685+nilsutuysuz@users.noreply.github.com>
2025-05-15 09:06:25 +0300

    Update wikidata.js
    Adding descriptionn of waste management

--- practice-app/frontend-web/src/services/wikidata.js
+++ practice-app/frontend-web/src/services/wikidata.js
@@ -51,4 +51,20 @@ export async function fetchWikidataDescriptionCircularEconomy(itemId = "Q1062957
     console.error(err);
     return "Failed to load circular economy description.";
   }
+  
+}
+export async function fetchWikidataDescriptionWasteManagement(itemId = "Q180388", language = "en") {
+  try {
+    const res = await fetch(`https://www.wikidata.org/wiki/Special:EntityData/${itemId}.json`);
+
+    if (!res.ok) throw new Error("Failed to fetch entity data");
+
+    const data = await res.json();
+    const description = data.entities[itemId].descriptions?.[language]?.value;
+
+    return description || "No description found.";
+  } catch (err) {
+    console.error(err);
+    return "Failed to load description.";
+  }
 }
