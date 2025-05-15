a79f686d24794e50d7f621e3e0e4286b531fe0c9
nilsutuysuz <162128685+nilsutuysuz@users.noreply.github.com>
2025-05-15 09:10:53 +0300

    Update InfoBox.js

--- practice-app/frontend-web/src/components/home/InfoBox.js
+++ practice-app/frontend-web/src/components/home/InfoBox.js
@@ -2,25 +2,31 @@ import React, { useEffect, useState } from 'react';
 import {
   fetchWikidataDescriptionRecycling,
   fetchWikidataDescriptionSustainableDevelopment,
+  fetchWikidataDescriptionWasteManagement,
   fetchWikidataDescriptionCircularEconomy,
 } from '../../services/wikidata';
 
 const InfoBox = () => {
   const [recyclingDesc, setRecyclingDesc] = useState("Loading recycling...");
   const [sustainableDesc, setSustainableDesc] = useState("Loading sustainable development...");
+  const [wasteManagementDesc, setWasteManagementDesc] = useState("Loading waste management...");
   const [circularEconomyDesc, setCircularEconomyDesc] = useState("Loading circular economy...");
 
   useEffect(() => {
     fetchWikidataDescriptionRecycling()
       .then(setRecyclingDesc)
       .catch(() => setRecyclingDesc("Failed to load recycling description."));
-
+   
     fetchWikidataDescriptionSustainableDevelopment()
       .then(setSustainableDesc)
       .catch(() => setSustainableDesc("Failed to load description."));
-      fetchWikidataDescriptionCircularEconomy()
-      .then(setCircularEconomyDesc)
-      .catch(() => setCircularEconomyDesc("Failed to load circular economy description."));
+     fetchWikidataDescriptionWasteManagement()
+      .then(setWasteManagementDesc)
+      .catch(() => setWasteManagementDesc("Failed to load waste management description."));
+
+    fetchWikidataDescriptionCircularEconomy()
+    .then(setCircularEconomyDesc)
+    .catch(() => setCircularEconomyDesc("Failed to load circular economy description."));
   }, []);
 
   return (
@@ -30,6 +36,8 @@ const InfoBox = () => {
 
       <h3>🧭 What is Sustainable Development?</h3>
       <p>{sustainableDesc}</p>
+      <h3>🔄 What is Waste Management?</h3>
+      <p>{wasteManagementDesc}</p>
       <h3>🔄 What is Circular Economy?</h3>
       <p>{circularEconomyDesc}</p>
     </div>
